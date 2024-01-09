03997ce1363e1d6b7bb9f6ab1ce89b827cb07978
841223 <madhupranesh291998@gmail.com>
2024-01-09 11:04:41 +0530

    Updated gitignore to read jar from target

---
+++ target/classes/users.sql
@@ -0,0 +1,5 @@
+insert into users(id,name,username,password) values(1,'Madhu Pranesh','madhu','$2a$12$LRpSghyXzzzsrNOF6OtVeeDUbanfztf80oER77qFlk1GfowgdaNZi');
+insert into users(id,name,username,password) values(2,'Jani','user1','$2a$12$LRpSghyXzzzsrNOF6OtVeeDUbanfztf80oER77qFlk1GfowgdaNZi');
+insert into users(id,name,username,password) values(3,'Deepak','user2','$2a$12$LRpSghyXzzzsrNOF6OtVeeDUbanfztf80oER77qFlk1GfowgdaNZi');
+insert into sample(id,name,college) values (1,'Madhu','KCT');
+insert into sample(id,name,college) values (2,'Pranesh','KCT');
